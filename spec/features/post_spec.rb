19b8f13342c329da8c6897accb58962be0fce506
pulgamecanica <pulgamecanica11@gmail.com>
2020-04-23 16:13:58 +0100

    Process for refactor

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
-    it 'can be reached successfully' do
-      visit posts_path
-      expect(page.status_code).to eq(200)
+	describe 'index' do
+	    it 'can be reached successfully' do
+	      visit posts_path
+	      expect(page.status_code).to eq(200)
+	    end
+
+	    it "has a title of Posts" do
+	    	visit posts_path
+	    	expect(page).to have_content(/Posts/)
+	    end
+	end
+    describe "creation" do
+    	before do
+    		visit new_post_path
+    	end
+    	it "has a new for that can be reached" do
+    		expect(page.status_code).to eq(200)
+    end
+    	it "can be created from new from page" do
+    		visit new_post_path
+
+    		fill_in "post[date]", with: Date.today
+    		fill_in "post[rationale]", with: "Some rationale"
+
+    		click_on "Save"
+    		expect(page).to have_content("Some rationale")
     end
 
-    it "has a title of Posts" do
-    	visit posts_path
-    	expect(page).to have_content(/Posts/)
-    end
   end
 end
