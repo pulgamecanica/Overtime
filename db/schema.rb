ae77f48fcb3744d73913d6f6a208abeee75505b5
pulgamecanica <pulgamecanica11@gmail.com>
2020-04-23 13:42:36 +0100

    Add Devise Autentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,60 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831143901) do
+ActiveRecord::Schema.define(version: 20200423103420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "audit_logs", id: :serial, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "status", default: 0
-    t.date "start_date"
-    t.date "end_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_audit_logs_on_user_id"
-  end
-
-  create_table "hands", id: :serial, force: :cascade do |t|
-    t.integer "hand_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", id: :serial, force: :cascade do |t|
-    t.date "date"
-    t.text "rationale"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "status", default: 0
-    t.decimal "overtime_request", default: "0.0"
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "users", id: :serial, force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "audit_logs", "users"
-  add_foreign_key "posts", "users"
 end
